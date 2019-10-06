8257c7894f7ef9276dedaaa7b29734a4a4217675
Swathi Shenoy M <swathishenoyms96@gmail.com>
2019-10-06 12:46:57 +0530

    Added login and logout components

--- firstproject/src/app/app.module.ts
+++ firstproject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent1 } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent1,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
